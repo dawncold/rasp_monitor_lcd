02a8aa042efdeb51e25c64f0809acfb0603d9139
TianZhen <loooseleaves@gmail.com>
2017-11-04 20:06:00 +0800

    feat: new method

--- src/rasp_monitor_lcd/main.py
+++ src/rasp_monitor_lcd/main.py
@@ -2,42 +2,36 @@
 from __future__ import unicode_literals, print_function, division
 
 import time
-
-import save_th
-import save_cqi
+import Adafruit_DHT
+import read_cqi
 import lcd
 
-line1 = None
-line2 = None
-
-
-def read_line1():
-    with open('/home/pi/{}'.format(save_th.TH_FILE_NAME)) as f:
-        return f.read()
-
-
-def read_line2():
-    with open('/home/pi/{}'.format(save_cqi.CQI_FILE_NAME)) as f:
-        return f.read()
-
+PIN = 4
 
 lcd.init_lcd()
 
 
+def get_th():
+    while 1:
+        try:
+            t, h = Adafruit_DHT.read_retry(Adafruit_DHT.DHT11, PIN)
+        except:
+            continue
+        if t and h:
+            return t, h
+
+
+def get_cqi():
+    while 1:
+        data = read_cqi.read_data()
+        if data:
+            return '{},{},{}'.format(data[str(read_cqi.P_C_PM10)], data[str(read_cqi.P_C_PM25)], data[str(read_cqi.P_C_PM100)])
+
+
 while 1:
-    line1_data = read_line1()
-    if line1 is None:
-        line1 = line1_data
-    elif line1 != line1_data:
-        line1 = line1_data
-
-    line2_data = read_line2()
-    if line2 is None:
-        line2 = line2_data
-    elif line2 != line2_data:
-        line2 = line2_data
-
+    line1 = get_th()
+    line2 = get_cqi()
     lcd.lcd_string(line1, lcd.LCD_LINE_1)
     lcd.lcd_string(line2, lcd.LCD_LINE_2)
 
-    time.sleep(15)
+    time.sleep(30)
