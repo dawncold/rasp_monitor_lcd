87037589b5eaff683024af5864c5524760f032b8
TianZhen <loooseleaves@gmail.com>
2017-11-04 16:45:14 +0800

    fix: syntax

--- src/rasp_monitor_lcd/save_cqi.py
+++ src/rasp_monitor_lcd/save_cqi.py
@@ -8,4 +8,6 @@ if __name__ == '__main__':
     data = read_cqi.read_data()
     if data:
         with open(CQI_FILE_NAME, mode='wb+') as f:
-            f.write('{},{},{}'.format(data[str(read_cqi.P_C_PM10)], data[str(read_cqi.P_C_PM25), data[str(read_cqi.P_C_PM100)]]).encode('UTF-8'))
+            f.write('{},{},{}'.format(data[str(read_cqi.P_C_PM10)],
+                                      data[str(read_cqi.P_C_PM25)],
+                                      data[str(read_cqi.P_C_PM100)]).encode('UTF-8'))
