c6795c07924893ff0b2cabcb1f70cd883b3d0c41
TianZhen <loooseleaves@gmail.com>
2017-11-05 22:38:59 +0800

    feat: daemonize for main module

--- src/rasp_monitor_lcd/main.py
+++ src/rasp_monitor_lcd/main.py
@@ -1,6 +1,8 @@
 # -*- coding: UTF-8 -*-
 from __future__ import unicode_literals, print_function, division
 
+import os
+import sys
 import time
 import Adafruit_DHT
 import read_cqi
@@ -27,13 +29,25 @@ def get_cqi():
         if data:
             return '{}, {}, {}'.format(data[str(read_cqi.P_C_PM10)], data[str(read_cqi.P_C_PM25)], data[str(read_cqi.P_C_PM100)])
 
+if __name__ == '__main__':
 
-while 1:
-    lcd.lcd_string('Reading h&t', lcd.LCD_LINE_1)
-    line1 = get_humidity_and_temperature()
-    lcd.lcd_string('Reading CQI...', lcd.LCD_LINE_2)
-    line2 = get_cqi()
-    lcd.lcd_string(line1, lcd.LCD_LINE_1)
-    lcd.lcd_string(line2, lcd.LCD_LINE_2)
+    pid = os.fork()
 
-    time.sleep(30)
+    if pid:
+        # parent process, exit
+        sys.exit(0)
+
+    # child process
+
+    os.umask(0)
+    os.setsid()
+
+    while 1:
+        lcd.lcd_string('Reading h&t', lcd.LCD_LINE_1)
+        line1 = get_humidity_and_temperature()
+        lcd.lcd_string('Reading CQI...', lcd.LCD_LINE_2)
+        line2 = get_cqi()
+        lcd.lcd_string(line1, lcd.LCD_LINE_1)
+        lcd.lcd_string(line2, lcd.LCD_LINE_2)
+
+        time.sleep(30)
